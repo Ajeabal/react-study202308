09144c90fd46963519e787e248df40ccf66a257d
SeungHanLee <swq3064@naver.com>
2023-08-11 09:30:03 +0900

    props 기초

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,31 +1,31 @@
 import React from "react";
 // css 로딩
 import "./ExpenseItem.css";
-const ExpenseItem = () => {
-  const expenseDate = new Date(2023, 8, 4);
-  const expenseName = "냠냠치킨";
-  const expensePrice = 19000;
-
+const ExpenseItem = ({ title, price: propsPrice, date }) => {
+  const price = 99999;
+  // const expenseDate = date;
+  // const expenseName = title;
+  // const expensePrice = propsPrice;
   //   한 자리 숫자를 2자리 숫자로 변환하는 함수
   const make2digit = (text) => {
     return text.toString().padStart(2, "0");
   };
   //   날짜 포맷팅 변환 함수 정의
   const makeFormattedDate = () => {
-    const year = expenseDate.getFullYear();
-    const month = expenseDate.getMonth();
-    const date = expenseDate.getDate();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
 
-    return `${year}년 ${make2digit(month)}월 ${make2digit(date)}일`;
+    return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
   };
   // 숫자 ,추가
-  const formattedPrice = new Intl.NumberFormat("ko-KR").format(expensePrice);
+  const formattedPrice = new Intl.NumberFormat("ko-KR").format(propsPrice);
 
   return (
     <div className="expense-item">
       <div>{makeFormattedDate()}</div>
       <div className="expense-item__description">
-        <h2>{expenseName}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{formattedPrice}원</div>
       </div>
     </div>
