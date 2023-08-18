80409b84e8c8fe3e2a9305e53ef3416887a13c70
SeungHanLee <swq3064@naver.com>
2023-08-18 09:24:09 +0900

    styled components 학습

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,33 @@
 import React from "react";
+// import "./Button.css";
+// 스타일 컴포넌츠 사용
+import styled from "styled-components";
 
-import "./Button.css";
+// const Button = ({ type, onClick, children }) => {
+//   return (
+//     <button type={type} className="button" onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// };
 
-const Button = ({ type, onClick, children }) => {
-  return (
-    <button type={type} className="button" onClick={onClick}>
-      {children}
-    </button>
-  );
-};
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+  &:focus {
+    outline: none;
+  }
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
 
 export default Button;
