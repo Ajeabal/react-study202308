b3967a6c196ab2d7d95b4396fe210b9bf34a2fb6
SeungHanLee <swq3064@naver.com>
2023-08-23 10:27:57 +0900

    useContext와 리펙토링

--- src/components/SideEffect/Home/Home.js
+++ src/components/SideEffect/Home/Home.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import Card from '../../UI/Card';
-import styles from './Home.module.css';
+import Card from "../../UI/Card";
+import styles from "./Home.module.css";
+import Button from "../../UI/Button/Button";
+import AuthContext from "../../../store/auth-context";
 
 const Home = () => {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Card className={styles.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
   );
 };
